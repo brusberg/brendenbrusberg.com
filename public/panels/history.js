fe1f7894dd2d852c6dff70a4fde7dabd26a31550
brusberg <brenden_brusberg@mckinsey.com>
2026-06-08 17:32:47 -0400

    fix: update history teacher title

--- public/panels/history.js
+++ public/panels/history.js
@@ -10,7 +10,7 @@
       'A personal history of teachers, work, poetry, nature, and the projects that shaped how I think. Plus an interactive <a href="https://en.wikipedia.org/wiki/Mandelbrot_set">Mandelbrot</a> and <a href="https://en.wikipedia.org/wiki/Julia_set">Julia</a> simulation.',
     storyHtml: [
       'I was born and raised in Sparta, New Jersey, in Sussex County. My brother and sister were 14 and 10 years older than me, so I grew up with their hand-me-down tech, old computer games, and N64 cartridges.',
-      'I have to thank a lot of teachers in high school who shaped how I see the world: <a href="https://www.linkedin.com/in/margaret-incantalupo-37a47a24/">Mrs. I</a>, who mentored me through high school, gave me room to learn on my own, and first introduced me to programming; Mr. Masker, who taught me that sometimes analog is the best way to learn; Mr. Rienstein, who taught me how practice, space, and peers nurture skill and craft; <a href="https://www.linkedin.com/in/kate-leblanc-475a9929/">Miss Leblanc</a>, who showed me how reading extends into your life and voice and also how to have a personality; and Mrs. Lovett, who showed me that work can feel like falling down the rabbit hole.',
+      'I have to thank a lot of teachers in high school who shaped how I see the world: <a href="https://www.linkedin.com/in/margaret-incantalupo-37a47a24/">Mrs. I</a>, who mentored me through high school, gave me room to learn on my own, and first introduced me to programming; Mr. Masker, who taught me that sometimes analog is the best way to learn; Mr. Rienstein, who taught me how practice, space, and peers nurture skill and craft; <a href="https://www.linkedin.com/in/kate-leblanc-475a9929/">Ms. LeBlanc</a>, who showed me how reading extends into your life and voice and also how to have a personality; and Mrs. Lovett, who showed me that work can feel like falling down the rabbit hole.',
       'I am glad I graduated from college before LLMs. I mainly studied computer science and wanted to be a software engineer. At 18, I joined McKinsey & Company as an SWE intern. A data scientist heard I was not doing much my first week and stole me. That was <a href="https://www.linkedin.com/in/dale-jacques/">Dale Jacques</a>, who quickly became my manager and mentor.',
       'I came back the next summer and never really left. I worked part-time as a permanent member of the Firm through school, three or four days a week during the semester. That work let me learn alongside excellent software engineers, designers, and statisticians, with the flexibility to explore. In school I started my master’s early and added a math minor, looking for stronger foundations for what I was already doing at work.',
       'What followed was that Dale and I became a kind of specialty shop, often taking on hard projects without templates or playbooks. We centered a lot of the work around reading the latest research papers for NLP, ML, and competitions.',
@@ -23,7 +23,7 @@
     linksLabel: 'Links',
     links: [
       ['Mrs. I', 'https://www.linkedin.com/in/margaret-incantalupo-37a47a24/'],
-      ['Miss Leblanc', 'https://www.linkedin.com/in/kate-leblanc-475a9929/'],
+      ['Ms. LeBlanc', 'https://www.linkedin.com/in/kate-leblanc-475a9929/'],
       ['Dale Jacques', 'https://www.linkedin.com/in/dale-jacques/'],
       ['Song of Myself', 'https://www.poetryfoundation.org/poems/45477/song-of-myself-1892-version'],
       ['Spring and All (Excerpt)', 'https://www.poetryfoundation.org/poems/148465/spring-and-all-xxv-somebody-dies-every-four-minutes'],
