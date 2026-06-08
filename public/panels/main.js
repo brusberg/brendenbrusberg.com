a31dc8b7c7ecaab6ba82c2cd32aec1edc6ff97a0
brusberg <brenden_brusberg@mckinsey.com>
2026-06-08 09:46:19 -0400

    fix: polish portfolio copy and art mobile

--- public/panels/main.js
+++ public/panels/main.js
@@ -5,7 +5,7 @@
     eyebrow: '',
     title: 'Brenden Brusberg',
     summary:
-      'I work on designing platforms to better understand and evaluate complex systems in organizations, leveraging multidisciplinary skills and teams to nurture real value.',
+      'I work on designing platforms to better understand and evaluate complex systems in organizations, leveraging multidisciplinary skills and teams to nurture real value. Plus a SimAnt-inspired interactive ant simulation.',
     storyHtml: [
       'One of the first computer games I played was <a href="https://en.wikipedia.org/wiki/SimAnt">SimAnt</a>. Even before I could read I would sneak onto the family computer where my much older brother had working DOS games like SimCity and SimAnt.',
       'This simulation is a recreation of what started my journey into computers : )'
